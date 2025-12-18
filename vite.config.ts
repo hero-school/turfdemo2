61c4c63d6985a37a5dad27eadae18c77a2ddf038
hero-school <hero1405psn@gmail.com>
2025-12-18 14:07:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/turfdemo2/',
+    base: '/turfdemo2/',   // <-- BELANGRIJK voor GitHub Pages
     server: {
       port: 3000,
       host: '0.0.0.0',
