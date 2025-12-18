05c8234ef79feb6fb3928ebbe66f6ec8c8251831
hero-school <hero1405psn@gmail.com>
2025-12-18 14:10:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, '.', '')
+
   return {
-    base: '/turfdemo2/',   // <-- BELANGRIJK voor GitHub Pages
+    base: '/turfdemo2/',
     server: {
       port: 3000,
       host: '0.0.0.0',
@@ -13,12 +14,12 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
-      }
-    }
-  };
-});
+      },
+    },
+  }
+})
